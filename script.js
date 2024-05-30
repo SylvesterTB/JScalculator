91574ce371af48d91f68d905ac6c43b724ac3d3f
SylvesterTB <139545276+SylvesterTB@users.noreply.github.com>
2024-05-30 17:18:17 +0000

    Working math with "eval"

--- script.js
+++ script.js
@@ -14,17 +14,26 @@ function add()
 function subtract()
 {
     const p_select = document.querySelector("p");
-    p_select.textContent += "+"
+    p_select.textContent += "-"
 }
 
 function multiply()
 {
     const p_select = document.querySelector("p");
-    p_select.textContent += "+"
+    p_select.textContent += "x"
 }
 
 function divide()
 {
     const p_select = document.querySelector("p");
-    p_select.textContent += "+"
+    p_select.textContent += "÷"
+}
+
+function result() {
+    const p_select = document.querySelector("p");
+    const expression = p_select.textContent;
+    const result = eval(expression);
+
+    const h3_select = document.querySelector("h3");
+    h3_select.textContent = "Result: " + result;
 }
